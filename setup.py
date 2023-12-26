d609e778486b057f9a43c93272edd70c0ab355ba
Shubham Aware <sandyaware07@gmail.com>
2023-12-26 19:42:47 +0530

    This is the first commit for creditcard fraud detection project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a demo project of Credit card Fraud detection with concepts of MLOps on real life dataset',
+    author='Shubham Aware',
+    license='',
+)
